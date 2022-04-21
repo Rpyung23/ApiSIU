abf87f4dbe7191ac15a3b16ed09ecb1fdf16dfbc
Angel Ramos <angelrc1998@hotmail.com>
2022-04-21 12:23:12 -0500

    apiSIU

--- config/conn.js
+++ config/conn.js
@@ -3,7 +3,11 @@ let mongoose = require('mongoose');
 const dbConnection = async ()=>{
     try {
 
-       var conn = await mongoose.connect('mongodb://localhost/siu',{
+       /*var conn = await mongoose.connect('mongodb://localhost/siu',{
+            useNewUrlParser:true,
+            useUnifiedTopology:true,
+        });*/
+        var conn = await mongoose.connect('mongodb+srv://root:fZc2frXKzV7RhZdr@cluster0.aihfo.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',{
             useNewUrlParser:true,
             useUnifiedTopology:true,
         });
