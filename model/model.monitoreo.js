4cf02a87def7735095061cd4fe80f15c184f6948
Rpyung23 <guaman1579@gmail.com>
2022-07-13 17:50:43 -0500

    asd

--- model/model.monitoreo.js
+++ model/model.monitoreo.js
@@ -9,7 +9,7 @@ class ModelMonitoreo {
             var con = await connections[i].promise();
             const data = await con.query("select CodiVehiMoni,UltiLatiMoni,UltiLongMoni,UltiRumbMoni,UltiVeloMoni," +
                 "UltiFechMoni from monitoreo WHERE LetrRutaMoni = '"+linea+"' and date(UltiFechMoni) between current_date() " +
-                "and current_date()");
+                "and current_date() and idSali_mMoni > 0");
             con.end();
             var datos = data[0]
             for (var j = 0; j < datos.length; j++) {
